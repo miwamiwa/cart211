752e18a1272b89e5c850822e66ed6d9eb13e05b4
miwamiwa <6parsam@gmail.com>
2018-12-06 21:15:08 -0500

    cleaning up webpage code

--- js/scriptshort.js
+++ js/scriptshort.js
@@ -4,85 +4,102 @@ var hi;
 var a, b, c, fact;
 var lastWidth, thisHeight, canvas;
 var defHeight=280;
+
 function setup(){
+
   lastWidth=window.innerWidth-20;
   background(255);
+
   if(window.innerHeight-100<defHeight){
     thisHeight=window.innerHeight-50;
-  } else{thisHeight=defHeight;}
+  }
+  else{
+    thisHeight=defHeight;
+  }
   canvas = createCanvas(lastWidth,thisHeight);
-canvas.parent('sketch-holder');
-frameRate(8);
-zed=14;
-bg=255;
-frame=0;
-diam=window.innerHeight/20;
-txtsize=window.innerHeight/10;
-background(bg);
-hi="";
-maxx=6;
-maxinit=maxx;
-mshapes=4;
-shape=0;
-textFont("Consolas");
-fct=40;
-}
+  canvas.parent('sketch-holder');
 
+  frameRate(8);
+
+  zed=14;
+  bg=255;
+  frame=0;
+  diam=window.innerHeight/20;
+  txtsize=window.innerHeight/10;
+  background(bg);
+  hi="";
+  maxx=6;
+  maxinit=maxx;
+  mshapes=4;
+  shape=0;
+  textFont('Inconsolata');
+  fct=40;
+}
 
 function draw(){
-if(lastWidth!=window.innerWidth-20){update();}
-if(shape==mshapes){
-fill(bg);
-noStroke();
-rect(0, 0, width, height);
-shape=0;
-mshapes=round(random(4, 12));
+
+  if(lastWidth!=window.innerWidth-20){
+    update();
+  }
+  if(shape==mshapes){
+    fill(bg);
+    noStroke();
+    rect(0, 0, width, height);
+    shape=0;
+    mshapes=round(random(4, 12));
+  }
+
+  frame+=1;
+  frame=constrain(frame, 0, maxx);
+
+  for(var i=0; i<frame; i++){
+    zed=zed*(-1);
+    noStroke();
+    var red=constrain(random(a-fct, a+fct), 0, 255);
+    var gre=constrain(random(b-fct, b+fct), 0, 255);
+    var blu=constrain(random(c-fct, c+fct), 0, 255);
+    fill(red, gre, blu);c
+    ellipse(rx+width/2+i,ry+ height/2+zed*i, diam+zed*i, diam+zed*i);
+  }
+
+  if(frame==maxx){
+    rx=round(random(-width/3,width/3));
+    ry=round(random(-height/3,height/3));
+    frame=0;
+    maxx=round(random(2, maxinit));
+    shape+=1;
+    a=random(0, 255);
+
+    // pick next starting colour
+    if(random()<0.5){
+      b=255-a+random(-30, 30);
+      c=random(255);
+    }
+    else {
+      c=255-a+random(-30, 30);
+      b=random(255);
+    }
+  }
+
+  fill(225+zed);
+  textSize(txtsize);
+  text(hi, width/2-hi.length/2+zed/5, height/2-txtsize/2+zed/5);
 }
 
-frame+=1;
-frame=constrain(frame, 0, maxx);
-
-for(var i=0; i<frame; i++){
-zed=zed*(-1);
-noStroke();
-
-var red=constrain(random(a-fct, a+fct), 0, 255);
-var gre=constrain(random(b-fct, b+fct), 0, 255);
-var blu=constrain(random(c-fct, c+fct), 0, 255);
-fill(red, gre, blu);c
-ellipse(rx+width/2+i,ry+ height/2+zed*i, diam+zed*i, diam+zed*i);
-}
-
-if(frame==maxx){
-rx=round(random(-width/3,width/3));
-ry=round(random(-height/3,height/3));
-frame=0;
-maxx=round(random(2, maxinit));
-shape+=1;
-a=random(0, 255);
-if(random()<0.5){
-  b=255-a+random(-30, 30);
-  c=random(255);
-} else {
-  c=255-a+random(-30, 30);
-  b=random(255);
-}
-
-}
-
-fill(225+zed);
-textSize(txtsize);
-text(hi, width/2-hi.length/2+zed/5, height/2-txtsize/2+zed/5);
-}
+// update canvas size
 
 function update(){
+  
   lastWidth=window.innerWidth-20;
   background(255);
   if(window.innerHeight-100<defHeight){
     thisHeight=window.innerHeight-50;
-  } else{thisHeight=defHeight;}
+  }
+  else{
+    thisHeight=defHeight;
+  }
   canvas = createCanvas(lastWidth,thisHeight);
-canvas.parent('sketch-holder');
+  canvas.parent('sketch-holder');
   frame=0;
   shape=0;
   diam=window.innerHeight/20;
@@ -90,16 +107,20 @@ canvas.parent('sketch-holder');
 }
 
 function mousePressed(){
-    console.log(mouseX+" "+mouseY);
-    if(mouseX>0&&mouseX<width&&mouseY>0&&mouseY<height){
-frame=0; rx=mouseX-width/2; ry=mouseY-height/2;
-}
-a=random(0, 255);
-if(random()<0.5){
-  b=255-a+random(-30, 30);
-  c=random(255);
-} else {
-  c=255-a+random(-30, 30);
-  b=random(255);
-}
+
+  console.log(mouseX+" "+mouseY);
+  if(mouseX>0&&mouseX<width&&mouseY>0&&mouseY<height){
+    frame=0;
+    rx=mouseX-width/2;
+    ry=mouseY-height/2;
+  }
+  a=random(0, 255);
+  if(random()<0.5){
+    b=255-a+random(-30, 30);
+    c=random(255);
+  }
+  else {
+    c=255-a+random(-30, 30);
+    b=random(255);
+  }
 }
--- js/scriptthree.js
+++ js/scriptthree.js
@@ -1,5 +1,7 @@
 var frame, zed, rx, ry, bg, diam, txtsize, maxx, shape, mshapes;
-var maxinit, r2x, r2y;
+var maxinit;
+var r2x;
+var r2y;
 var hi;
 var frame2, maxx2;
 var e1x= [0];
@@ -22,97 +24,102 @@ function setup(){
     thisHeight=window.innerHeight-50;
   } else{thisHeight=500;}
   canvas = createCanvas(lastWidth,thisHeight);
-canvas.parent('sketch-holder');
-frameRate(8);
-zed=18;
-bg=255;
-frame=1; frame2=0;
-diam=window.innerHeight/10;
-txtsize=window.innerHeight/10;
-background(bg);
-fill(85);
-textSize(14);
-text("click on canvas to refresh", width-200, height);
-hi="final";
-maxinit=8;
-maxx=round(random(4, maxinit));;
-maxx2=round(random(4, maxinit));;
-e1x= [maxx];
-e1y=[maxx];
-e1d= [maxx];
-e2x= [maxx];
-e2y= [maxx];
-e2d= [maxx];
-mshapes=4;
-shape=0;
-textFont("Consolas");
+  canvas.parent('sketch-holder');
+  frameRate(8);
+  zed=18;
+  bg=255;
+  frame=1; frame2=0;
+  diam=window.innerHeight/10;
+  txtsize=window.innerHeight/10;
+  background(bg);
+  fill(85);
+  textSize(14);
+  text("click on canvas to refresh", width-200, height);
+  hi="final";
+  maxinit=8;
+  maxx=round(random(4, maxinit));;
+  maxx2=round(random(4, maxinit));;
+  e1x= [maxx];
+  e1y=[maxx];
+  e1d= [maxx];
+  e2x= [maxx];
+  e2y= [maxx];
+  e2d= [maxx];
+  mshapes=4;
+  shape=0;
+  textFont('Inconsolata');
 
-if(random()<0.5){
-  rx=random(500,width-width/8);
-  ry=random(height/8,height-height/8);
-}else{
-  rx=random(width/8,500);
-  ry=random(200,height-height/8);
-}
-if(random()<0.5){
-  r2x=random(500,width-width/8);
-  r2y=random(height/8,height-height/8);
-}else{
-  r2x=random(width/8,500);
-  r2y=random(200,height-height/8);
-}
+
+  if(random()<0.5){
+    rx=random(500,width-width/8);
+    ry=random(height/8,height-height/8);
+  }
+  else{
+    rx=random(width/8,500);
+    ry=random(200,height-height/8);
+  }
+  if(random()<0.5){
+    r2x=random(500,width-width/8);
+    r2y=random(height/8,height-height/8);
+  }
+  else{
+    r2x=random(width/8,500);
+    r2y=random(200,height-height/8);
+  }
+
 
 }
 
 
 function draw(){
-if(lastWidth!=window.innerWidth-20){
-  update();
-}
+  if(lastWidth!=window.innerWidth-20){
+    update();
+  }
 
-if(frame>maxx){shape1done=true;
-}
+  if(frame>maxx){
+    shape1done=true;
+  }
 
   if(!shape1done) { frame+=1;
   }
-for(var i=0; i<frame; i++){
-  if(frame<maxx){
-zed=zed*(-1);
-noStroke();
-fill(random(235, 255),random(0, 95),random(0, 95));
-ellipse(rx+i,ry+zed*i, diam+zed*i, diam+zed*i);
-e1x[i]=rx+i;
-e1y[i]=ry+zed*i;
-e1d[i]=abs(diam+zed*i);
-fill(150+2*zed);
-textSize(txtsize);
-text("midterm", e1x[i]+e1d[i]/2+20, e1y[i]+20);
-textSize(12);
-//text("click objects to follow link \nclick elsewhere to shuffle objects",width-250, height/2-24*i);
-}
-}
+  for(var i=0; i<frame; i++){
+    if(frame<maxx){
+      zed=zed*(-1);
+      noStroke();
+      fill(random(235, 255),random(0, 95),random(0, 95));
+      ellipse(rx+i,ry+zed*i, diam+zed*i, diam+zed*i);
+      e1x[i]=rx+i;
+      e1y[i]=ry+zed*i;
+      e1d[i]=abs(diam+zed*i);
+      fill(150+2*zed);
+      textSize(txtsize);
+      text("midterm", e1x[i]+e1d[i]/2+20, e1y[i]+20);
+      textSize(12);
+      //text("click objects to follow link \nclick elsewhere to shuffle objects",width-250, height/2-24*i);
+    }
+  }
 
-if(!shape2done) {
-frame2+=1;
-}
-if(frame2<=maxx2){
-  if(frame2<maxx2){
+  if(!shape2done) {
+    frame2+=1;
+  }
+  if(frame2<=maxx2){
+    if(frame2<maxx2){
 
-for(var j=0; j<frame2; j++){
-zed=zed*(-1);
-noStroke();
-fill(random(0, 95),random(0, 95),random(235, 255));
-ellipse(r2x+j, r2y+ zed*j, diam+zed*j, diam+zed*j);
-e2x[j]=r2x+j;
-e2y[j]=r2y+zed*j;
-e2d[j]=abs(diam+zed*j);
-fill(150+zed*2);
-textSize(txtsize);
-var texties="exam";
-text(texties, e2x[j]-e2d[j]/2-20-textWidth(texties), e2y[j]-20);
-}
-}
-}
+      for(var j=0; j<frame2; j++){
+        zed=zed*(-1);
+        noStroke();
+        fill(random(0, 95),random(0, 95),random(235, 255));
+        ellipse(r2x+j, r2y+ zed*j, diam+zed*j, diam+zed*j);
+        e2x[j]=r2x+j;
+        e2y[j]=r2y+zed*j;
+        e2d[j]=abs(diam+zed*j);
+        fill(150+zed*2);
+        textSize(txtsize);
+        var texties="exam";
+        text(texties, e2x[j]-e2d[j]/2-20-textWidth(texties), e2y[j]-20);
+      }
+    }
+  }
 
 
 
@@ -125,13 +132,17 @@ function update(){
 
   if(window.innerHeight-100<500){
     thisHeight=window.innerHeight-50;
-  } else{thisHeight=500;}
+  }
+  else{
+    thisHeight=500;
+  }
   canvas = createCanvas(lastWidth,thisHeight);
-canvas.parent('sketch-holder');
-textFont("Consolas");
-fill(85);
-textSize(14);
-text("click on canvas to refresh", width-200, height);
+  canvas.parent('sketch-holder');
+  textFont("Consolas");
+
+  fill(85);
+  textSize(14);
+  text("click on canvas to refresh", width-200, height);
   frame=0;
   shape1done=false;
   shape2done=false;
@@ -148,43 +159,48 @@ text("click on canvas to refresh", width-200, height);
   e2d= [maxx];
   mshapes=4;
   shape=0;
+
   if(random()<0.5){
-    rx=random(500,width-width/8);
-    ry=random(height/8,height-height/8);
+    rx=random(500,width-width/4);
+    ry=random(height/8,height-height/4);
   }else{
     rx=random(width/8,500);
     ry=random(200,height-height/8);
   }
   if(random()<0.5){
-    r2x=random(500,width-width/8);
-    r2y=random(height/8,height-height/8);
+    r2x=random(rx+100,width-width/16);
+    r2y=random(ry+100,height-height/16);
   }else{
-    r2x=random(width/8,500);
-    r2y=random(200,height-height/8);
+    r2x=random(width/16,rx);
+    r2y=random(ry,height-height/16);
   }
+
 }
 
 function mousePressed(){
-  console.log(mouseX+"  "+mouseY);
-var mouseElsewhere=true;
+
+  var mouseElsewhere=true;
   for(var i=0; i<frame; i++){
     if(dist(mouseX,mouseY,e1x[i],e1y[i]) <e1d[i]/2&&(mouseX>500||mouseY>180)){
-link="midterm/index.html"
-  windowObjectReference = window.open(link, "yo", strWindowFeatures);
-  mouseElsewhere=false;
-}
-}
-for(var i=0; i<frame2; i++){
-  if(dist(mouseX,mouseY,e2x[i],e2y[i]) <e2d[i]/2&&mouseX>500&&mouseY>180){
-link="final/index.html"
-windowObjectReference = window.open(link, "yo", strWindowFeatures);
-mouseElsewhere=false;
-link="";
-}
-}
-if(mouseElsewhere){
-  if(mouseX<width&&mouseX>0&&mouseY<height&&mouseY>0){
-update();
-}
-}
+      link="midterm/index.html"
+      windowObjectReference = window.open(link, "yo", strWindowFeatures);
+      mouseElsewhere=false;
+    }
+  }
+
+  for(var i=0; i<frame2; i++){
+    if(dist(mouseX,mouseY,e2x[i],e2y[i]) <e2d[i]/2&&mouseX>500&&mouseY>180){
+      link="final/index.html"
+      windowObjectReference = window.open(link, "yo", strWindowFeatures);
+      mouseElsewhere=false;
+      link="";
+    }
+  }
+
+  if(mouseElsewhere){
+    if(mouseX<width&&mouseX>0&&mouseY<height&&mouseY>0){
+      update();
+    }
+  }
+
 }
